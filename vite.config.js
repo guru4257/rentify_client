e70f70c497922862cbcb9c474b3dc6603c219f11
GuruprasathManikandan <guruprasath@saptanglabs.com>
2024-05-26 03:04:01 +0530

    commented url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 
-server_url = import.meta.env.VITE_SERVER_URL
+// server_url = import.meta.env.VITE_SERVER_URL
 export default defineConfig({
   server: {
     proxy: {
