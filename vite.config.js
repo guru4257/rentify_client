c2e95e4b9c18e7412663758952b4a02aa94a7f92
GuruprasathManikandan <guruprasath@saptanglabs.com>
2024-05-26 02:54:30 +0530

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+
+server_url = import.meta.env.VITE_SERVER_URL
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: server_url,
         secure: false,
       },
     },
